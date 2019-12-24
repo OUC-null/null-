2facaf8eff28e20636464d29b5a2905148d741f2
XIONGDianDian <49612861+XIONGDianDian@users.noreply.github.com>
2019-12-24 08:58:54 +0800

    Add files via upload

--- secondhand/src/service/StuffService.java
+++ secondhand/src/service/StuffService.java
@@ -7,17 +7,30 @@ public class StuffService {
 	private StuffDao stuffDao=new StuffDao();
 	//修改
 		public int chg(int stuffid , Stuff stuff){
-			//根据修改后的菜品名修改
-			Stuff s = stuffDao.findByStuffname(stuff.getStuffname());
-			//如果能够查到，查到的ID要与修改的ID不一致，不允许修改
-			if(s != null && s.getId() != stuffid){
+		//根据修改后的菜品名修改
+		Stuff s = stuffDao.findByStuffname(stuff.getStuffname());
+		//如果能够查到，查到的ID要与修改的ID不一致，不允许修改
+		if(s != null && s.getId() != stuffid){
 				return -1;
-			}
-			return stuffDao.chg(stuffid, stuff);
+		}
+		return stuffDao.chg(stuffid, stuff);
 		}
 	public class StuffService {
 	//删除已发布商品信息
 	public int del(int stuffid) {
 		return stuffDao.del(stuffid);
+	}
+	public ArrayList<ShowStuff> findByStuffname(String stuffname){
+		return stuffDao.findByStuffname(stuffname);
+	}
+	
+	//无条件查询
+	public ArrayList<ShowStuff> findAll(){
+		return stuffDao.findAll();
+	}
+	
+	//模糊查询
+	public ArrayList<ShowStuff> fuzzySearch(String userInput){
+		return stuffDao.fuzzySearch(userInput);
 	}
 }
